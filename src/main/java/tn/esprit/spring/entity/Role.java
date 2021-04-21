cffeba6be6dde15bbbd43d7401f28c686cf912fe
Dhaker <karousdhaker@gmail.com>
2021-04-21 09:33:46 +0100

    Json web Token (JWT)
    update of user 
    authentification

--- src/main/java/tn/esprit/spring/entity/Role.java
+++ src/main/java/tn/esprit/spring/entity/Role.java
@@ -1,64 +1,38 @@
 package tn.esprit.spring.entity;
+import javax.persistence.*;
 
-import java.io.Serializable;
+@Entity
+@Table(name = "roles")
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20)
+	private ERole name;
 
-		@Entity
-		@Table(name= "ROLE")
-		public class Role implements Serializable {
-			private static final long serialVersionUID = 1L;
-			@Id
-			@GeneratedValue (strategy= GenerationType.IDENTITY)
-			private long id_Role;
-			
-			private String role_name;
-			private String role_desc;
-			public long getId_Role() {
-				return id_Role;
-			}
-			public void setId_Role(long id_Role) {
-				this.id_Role = id_Role;
-			}
-			public String getRole_name() {
-				return role_name;
-			}
-			public void setRole_name(String role_name) {
-				this.role_name = role_name;
-			}
-			public String getRole_desc() {
-				return role_desc;
-			}
-			public void setRole_desc(String role_desc) {
-				this.role_desc = role_desc;
-			}
-			public static long getSerialversionuid() {
-				return serialVersionUID;
-			}
-			@Override
-			public String toString() {
-				return "Role [id_Role=" + id_Role + ", role_name=" + role_name + ", role_desc=" + role_desc + "]";
-			}
-			public Role(long id_Role, String role_name, String role_desc) {
-				super();
-				this.id_Role = id_Role;
-				this.role_name = role_name;
-				this.role_desc = role_desc;
-			}
-			public Role() {
-				super();
-				// TODO Auto-generated constructor stub
-			}
-			public Role(Role r){
-				this.role_name = r.role_name;
-				this.role_desc = r.role_desc;
-			}
-		
-	
+	public Role() {
 
+	}
+
+	public Role(ERole name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public ERole getName() {
+		return name;
+	}
+
+	public void setName(ERole name) {
+		this.name = name;
+	}
 }
